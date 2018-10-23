9e1bf3885053e282891ac9f36a6beda5abbcb51e
Axel3 <zabi-zabi-zabi@rambler.ru>
2018-10-23 21:50:27 +0300

    FirstCom

---
+++ GeometryTest/GeometryTest.Core/ShapeBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometryTest.Core
+{
+    public abstract class ShapeBase
+    {
+        public abstract double GetPerimeter();
+
+        public abstract double GetSquare();
+    }
+}
